8131e49f6bd41d4a925d357808e4261971f2a8b7
kevinlp9 <atilano.kevin.h17@gmail.com>
2025-09-02 13:04:24 -0600

    2/9/25

--- src/TestMain/TestMain.java
+++ src/TestMain/TestMain.java
@@ -1,4 +1,16 @@
 package TestMain;
 
+import Classes.Leon;
+import Classes.Tigre;
+
 public class TestMain {
+    public static void main(String[] args) {
+        Leon leon = new Leon("Terranova", 1.80f, 100f, 100f, "Leon", 100, 100, 100, 100);
+        leon.comer();
+        leon.dormir();
+        leon.comunicarse();
+        leon.correr();
+
+
+    }
 }
